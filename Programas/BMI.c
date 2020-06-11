3328762cc2748915e149581e4b8804cb5c1162e5
Juan <Juan010>
2020-06-11 01:00:52 -0300

    Update 11/6 1:00 AM

--- Programas/BMI.c
+++ Programas/BMI.c
@@ -7,7 +7,7 @@ int main(void)
 	printf("Calculadora de Indice de Masa Corporal\n\n");
 	printf("El objetivo de este programa es determinar\n");
 	printf("su IMC para comparación con los datos provistos\n");
-	printf("por <inserte nombre aqui>.\n\n");
+	printf("por el Departamento de Salud y Derechos Humanos.\n\n");
 	printf("Presione enter para continuar...\n");
 	getchar();
 
@@ -21,6 +21,10 @@ int main(void)
 	bmi = weight / (heightInM*heightInM);
 	printf("\nSu Indice de Masa Corporal es de %.2f\n\n",bmi);
 
-	printf("Puede comparar su resultado con la siguiente tabla:\n");
+	printf("Puede comparar su resultado con la siguiente tabla:\n\n");
+	printf("Bajopeso: Menor a 18.5\n");
+	printf("Peso Normal: Entre 18.5 y 25\n");
+	printf("Sobrepeso: Mayor a 25\n");
+	return 0;
 }
 	
